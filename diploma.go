a969e4083bc398c154b74404f5a4f8aa0bb6bd28
Danyla Dmn <danila.dmn@gmail.com>
2014-05-19 18:21:58 +0000

    main loop

--- diploma.go
+++ diploma.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-//    "fmt"
+    "fmt"
     "time"
     "flag"
+    "strconv"
+    "os"
+    "os/signal"
     "github.com/xdmn/diploma/modules"
 )
 
@@ -24,11 +27,40 @@ func init() {
 
 func main() {
     flag.Parse()
+    itoa := strconv.Itoa
+//    atoi := strconv.Atoi
 
-    //log is function
+
+//Logging
     log := modules.LogInit(LOGGING)
+    log("\n*** Log file ***")
+    t:=time.Now().Local()
+    const layout = "Jan 2, 2006 at 3:04pm"
+    log(t.Format(layout))
+    log("Network:")
+    log("    "+itoa(CLIENTS)+" client(s);")
+    log("    "+itoa(NODES)+  " node(s);")
+    log("    "+itoa(SERVERS)+" server(s).")
+//
 
-    log("*** Log file ***")
+//Interrupt chan
+    interruptChan := make(chan os.Signal, 1)
+    signal.Notify(interruptChan, os.Interrupt)
+
+
+//Main loop
+    for {
+        select {
+            case <-interruptChan :
+                fmt.Println("\nCTRL=C: Stop.")
+                log("--End--\n")
+                time.Sleep(150*time.Millisecond)
+                return
+            default:
+            }
+    }
+
+
 
     modules.Cmd("Hello, xdmn!")
     modules.Client("Hello, client!")
@@ -36,7 +68,5 @@ func main() {
     modules.Server("Hello, server!")
     
     
-    //tmp
-    time.Sleep(150*time.Millisecond)
     
 }
